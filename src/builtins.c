57932109e41ccae0e0479cf3f8c83819e23d2d0e
Alexandre Ouellet Fournier <aouellet@c2r3p7.42quebec.com>
2023-11-16 17:52:52 -0500

    fixed CD

--- src/builtins.c
+++ src/builtins.c
@@ -233,19 +233,26 @@ void update_cwd(t_ms*s_ms,char*path,char*old_path)
 	
 	var = ft_strjoin("PWD=",path); 
 	index = get_var_index("PWD",s_ms->env);
-	if(index == -1)
-		s_ms->env = add_var(var,s_ms->env); //keep or not
-	else
+	if(index != -1)
 		replace_var(s_ms->env,index,var);
 
 	var = ft_strjoin("OLDPWD=",old_path); 
 	index = get_var_index("OLDPWD",s_ms->env);
-	if(index == -1)
-		s_ms->env = add_var(var,s_ms->env);
-	else
+	if(index != -1)
 		replace_var(s_ms->env,index,var);
 
-	
+}
+
+char* ft_getenv(char**env)
+{
+	char *path;
+
+	path = get_var_string("HOME",env);
+	if(path)
+	{
+		return (path + get_char_index(path,'=') + 1);
+	}
+	return NULL;
 }
 
 int ft_cd(char**cmd,t_ms*s_ms) //deal with cd - ?
@@ -258,18 +265,25 @@ int ft_cd(char**cmd,t_ms*s_ms) //deal with cd - ?
 	if(cmd[1] && cmd[2])
 		ft_raise_err("too much args\n",-1);
 	if(!cmd[1] || (cmd[1][0] == '~' && !cmd[1][1]))
-		path = getenv("HOME"); //take home from copy instead <---
+	{
+		path = ft_getenv(s_ms->env);
+		if(!path)
+		{
+			printf("cd: HOME not set\n");
+			return -1;
+		}
+	}
 	else
 		path = cmd[1];
 	if(!getcwd(oldpwd,PATH_MAX))
 		return -1;
-
 	if(chdir(path) != 0)
+	{
+		printf("cd: no such file or directory: %s\n",path);
 		return -1;
+	}
 	if(!getcwd(pwd,PATH_MAX))
 		return -1;
-	
 	update_cwd(s_ms,pwd,oldpwd);
-	
 	return 0;
 }
--- src/main.c
+++ src/main.c
@@ -73,7 +73,7 @@ int	main(int argc, char **argv, char *const envp[])
 			// Parse Raw input
 			// Create tokens from raw line
 			t_ms_token *token_list = ft_tokenize(line);
-			print_tokens(token_list,line);
+			//print_tokens(token_list,line);
 
 			// Create t_exec_node list from tokens
 			exec_list = ft_init_exec_list(token_list);
